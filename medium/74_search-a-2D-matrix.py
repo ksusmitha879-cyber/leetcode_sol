dad9f8f480d24e8183742d4cd4f749683737f531
ksusmitha879-cyber <ksusmitha879@gmail.com>
2026-07-01 17:42:07 +0530

    Add search a 2D matrix medium - 009

---
+++ medium/74_search-a-2D-matrix.py
@@ -0,0 +1,17 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        n,m=len(matrix),len(matrix[0])
+        l,h=0,(n*m)-1
+        if not matrix or not matrix[0]:
+            return False
+        while(l<=h):
+            mid=l+(h-l)//2
+            mid_row, mid_col = divmod(mid, m)
+            val=matrix[mid_row][mid_col]
+            if val==target:
+                return True
+            elif val<=target:
+                l=mid+1
+            else:
+                h=mid-1
+        return False
