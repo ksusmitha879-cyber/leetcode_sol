c007af9494cddd96b7d067f175ea1d58886df5e2
ksusmitha879-cyber <ksusmitha879@gmail.com>
2026-07-03 18:29:02 +0530

    Add reverse pairs hard- 001

---
+++ hard/493_reverse-pairs..py
@@ -0,0 +1,22 @@
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        return self.merge_sort(nums, 0, len(nums) - 1)
+
+    def merge_sort(self, nums, left, right):
+        if left >= right:
+            return 0
+        
+        mid = (left + right) // 2
+        count = self.merge_sort(nums, left, mid) + self.merge_sort(nums, mid + 1, right)
+        
+        j = mid + 1
+        for i in range(left, mid + 1):
+            while j <= right and nums[i] > 2 * nums[j]:
+                j += 1
+            count += (j - (mid + 1))
+        
+    
+        nums[left:right + 1] = sorted(nums[left:right + 1])
+        return count
