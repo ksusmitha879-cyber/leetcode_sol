60c07528fe4c2f79166db873ff21cd338e3a3d0b
ksusmitha879-cyber <ksusmitha879@gmail.com>
2026-07-20 23:06:48 +0530

    Add lowest common ancestor of a bst medium - 016

--- medium/18_4sum.py
+++ medium/18_4sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]: # type: ignore
         n=len(nums)
         ans=[]
         nums.sort()
