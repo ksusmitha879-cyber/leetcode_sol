f6736d450dc23588ccb937ff2cf487c72bdc73da
ksusmitha879-cyber <ksusmitha879@gmail.com>
2026-08-04 19:53:08 +0530

    Add binary tree preorder traversal  easy - 023

--- easy/144_binary-tree-preorder-traversal.py
+++ easy/144_binary-tree-preorder-traversal.py
@@ -1,9 +1,9 @@
 class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]: # type: ignore
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]: # type: ignore
         def Tree(root,result):
             if root!=None:
-                Tree(root.left,result)
                 result.append(root.val)
+                Tree(root.left,result)
                 Tree(root.right,result)
         result=[]
         Tree(root,result)
