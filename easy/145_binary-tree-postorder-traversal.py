a7a9ca98d148ee4daebb958ef934c931dc30cb40
ksusmitha879-cyber <ksusmitha879@gmail.com>
2026-08-04 19:54:42 +0530

    Add binary tree postorder traversal  easy - 024

---
+++ easy/145_binary-tree-postorder-traversal.py
@@ -0,0 +1,11 @@
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        def Tree(root,result):
+            if root!=None:
+                
+                Tree(root.left,result)
+                Tree(root.right,result)
+                result.append(root.val)
+        result=[]
+        Tree(root,result)
+        return result
