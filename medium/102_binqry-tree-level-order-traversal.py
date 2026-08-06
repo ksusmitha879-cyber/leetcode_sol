d0eaa087c0ddc7713029fdea69dd3cd4b5fbfd5a
ksusmitha879-cyber <ksusmitha879@gmail.com>
2026-08-06 23:33:02 +0530

    Add binary tree level order traversal  medium - 025

---
+++ medium/102_binqry-tree-level-order-traversal.py
@@ -0,0 +1,16 @@
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]: # type: ignore
+        if not root:
+            return []
+        res=[]
+        queue=deque([root]) # type: ignore
+        while queue:
+            level_size=len(queue)
+            current_level=[]
+            for _ in range(level_size):
+                node=queue.popleft()
+                current_level.append(node.val)
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+            res.append(current_level)
+        return res
